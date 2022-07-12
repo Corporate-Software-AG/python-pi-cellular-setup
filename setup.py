c42787a88303d17bc3d811fa32f464145b22f06d
Thomas <65050898+tvonment@users.noreply.github.com>
2022-07-12 15:19:59 +0000

    add service

--- setup.py
+++ setup.py
@@ -1,16 +1,36 @@
 import os
 import time
 
+def get_status():
+    try:
+        return os.popen('sudo qmicli -d /dev/cdc-wdm0 --dms-get-operating-mode').read().split("\n\t")[1].split("'")[1]
+    except:
+        time.sleep(2)
+        print("QMI Interface: Get Status Retry")
+        return get_status()
+
+def startup_comm():
+    try:
+        startupcommand = 'sudo qmicli -d /dev/cdc-wdm0 --dms-set-operating-mode="online"'
+        os.system(startupcommand)
+    except:
+        time.sleep(2)
+        print("QMI Interface: Starte Modem Retry")
+        startup_comm()
+
+
 def bring_sim7600_up():
-    getstatus = os.popen('sudo qmicli -d /dev/cdc-wdm0 --dms-get-operating-mode').read().split("\n\t")[1].split("'")[1]
+    
+    print("QMI Interface: Get Status")
+    getstatus = get_status()
+
     if getstatus == 'online':
         print("QMI Interface: Gerät ist online")
     else:
         print("QMI Interface: Offline! ...starte Modem")
-        startupcommand = 'sudo qmicli -d /dev/cdc-wdm0 --dms-set-operating-mode="online"'
-        os.system(startupcommand)
+        startup_comm()
         time.sleep(2)
-        getstatus2 = os.popen('sudo qmicli -d /dev/cdc-wdm0 --dms-get-operating-mode').read().split("\n\t")[1].split("'")[1]
+        getstatus2 = get_status()
         if getstatus2 == 'online':
             print("QMI Interface: Gerät ist nun online")
         else:
@@ -18,23 +38,42 @@ def bring_sim7600_up():
             
         
 def set_raw_ip_mode():
-    com0 = 'sudo ip link set wwan0 down'
-    com1 = 'echo Y | sudo tee /sys/class/net/wwan0/qmi/raw_ip'
-    com2 = 'sudo ip link set wwan0 up'
-    print("QMI Interface: Set RAW IP Mode")
-    os.system(com0)
-    os.system(com1)
-    os.system(com2)
-    print("QMI Interface: Set RAW IP Mode complete")
+    try:
+        com0 = 'sudo ip link set wwan0 down'
+        com1 = 'echo Y | sudo tee /sys/class/net/wwan0/qmi/raw_ip'
+        com2 = 'sudo ip link set wwan0 up'
+        print("QMI Interface: Set RAW IP Mode")
+        os.system(com0)
+        os.system(com1)
+        os.system(com2)
+        print("QMI Interface: Set RAW IP Mode complete")
+    except:
+        print("QMI Interface: Set RAW IP Mode Retry")
+        time.sleep(2)
+        set_raw_ip_mode()
+
     
 def connect_qmi():
-    com_connect = 'sudo qmicli --device=/dev/cdc-wdm0 --device-open-proxy --wds-start-network="ip-type=4,apn=gprs.swisscom.ch" --client-no-release-cid'
-    com_dhcp = 'sudo udhcpc -i wwan0'
-    print("QMI Interface: Connect QMI")
-    os.system(com_connect)
-    print("QMI Interface: Connect start wwan0")
-    os.system(com_dhcp)
-     
+    try:
+        com_connect = 'sudo qmicli --device=/dev/cdc-wdm0 --device-open-proxy --wds-start-network="ip-type=4,apn=gprs.swisscom.ch" --client-no-release-cid'
+        print("QMI Interface: Connect QMI")
+        os.system(com_connect)
+    except:
+        print("QMI Interface: Connect QMI failed")
+        time.sleep(2)
+        connect_qmi()
+
+def connect_dhcp():
+    try:
+        com_dhcp = 'sudo udhcpc -i wwan0'
+        print("QMI Interface: Connect start wwan0")
+        os.system(com_dhcp)
+    except:
+        print("QMI Interface: Connect DBCH retry")
+        time.sleep(2)
+        connect_dhcp()
+
+
 if __name__ == "__main__":
     print("QMI Interface: Start Setup")
     bring_sim7600_up()
@@ -42,4 +81,5 @@ if __name__ == "__main__":
     set_raw_ip_mode()
     time.sleep(2)
     connect_qmi()
+    connect_dhcp()
     print("QMI Interface: Setup Complete")
